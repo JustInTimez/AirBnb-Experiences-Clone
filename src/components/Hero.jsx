a43c3ee41890a7bcda4077bcb470f85b481924a3
JustInTimez <just.oost@gmail.com>
2023-06-02 17:44:47 +0200

    Adds image and text for component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img className="hero-photo" src="images/photo-grid.png" alt="Photos of people and things" />
+            <h1 className="hero-heading">Check these out!</h1>
+            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
