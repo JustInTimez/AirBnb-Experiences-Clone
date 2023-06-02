7b756f456363ce91e3a466012bafdd78b50fffc6
JustInTimez <just.oost@gmail.com>
2023-06-02 21:01:39 +0200

    Replace values with props

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,14 @@ function App() {
     <>
       <Navbar />
       {/* <Hero /> */}
-      <Card />
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Some things Katie Zafs says. Aweh."
+        price={50}
+      />
 
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,15 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img className="card-image" src="/images/katie-zaferes.png" alt="Katie Zaferes" />
+            <img className="card-image" src={`/images/${props.img}`} alt="Katie Zaferes" />
             <div className="card-info">
                 <img className="card-star" src="/images/card/star.png" alt="Favoriting Star" />
-                <span>5.0</span>
-                <span className="dull">(6)• </span>
-                <span className="dull">USA</span>
+                <span>{props.rating}</span>
+                <span className="dull">({props.reviewCount}) •</span>
+                <span className="dull">{props.country}</span>
             </div>
             <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
