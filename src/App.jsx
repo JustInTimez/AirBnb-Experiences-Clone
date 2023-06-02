08f60af972ed830bb574505f24be4fc8e9d139c6
JustInTimez <just.oost@gmail.com>
2023-06-02 19:42:16 +0200

    Adds elements for card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from "react"
 import "./App.css"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <Card />
 
     </>
   )
