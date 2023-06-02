35bd655c429658e799264a1133d8924d5766d996
JustInTimez <just.oost@gmail.com>
2023-06-02 22:10:47 +0200

    Adds if slots are open badge to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
 import React from "react"
 
 export default function Card(props) {
-    console.log(props)
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <div className="card-badge">SOLD OUT</div>
             <img className="card-image" src={`/images/${props.img}`} alt="Katie Zaferes" />
             <div className="card-info">
                 <img className="card-star" src="/images/card/star.png" alt="Favoriting Star" />
