0ebdbf014c93f7c993a12cdab16e804f2a863997
JustInTimez <just.oost@gmail.com>
2023-06-05 13:31:32 +0200

    Adds basic footer
    Added some page links, but they are obviously not going to work, they are for decoration

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react"
 export default function Footer() {
   return (
     <footer>
-      <img src="/images/airbnb-logo.png" alt="Airbnb Logo" className="footer-logo" />
       <nav>
         <a href="#">Home</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
+        <img src="/images/airbnb-logo.png" alt="Airbnb Logo" className="footer-logo" />
       </nav>
     </footer>
   )
